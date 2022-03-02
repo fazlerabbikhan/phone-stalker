d9e1982175adf2c8a93169357632b384e7423465
Fazle Rabbi <you@example.com>
2022-03-02 15:29:32 +0600

    almost done

--- js/phones.js
+++ js/phones.js
@@ -1,13 +1,13 @@
 const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    // console.log(searchText);
+    console.log(searchText);
     searchField.value = '';
 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
         .then(res => res.json())
-        .then(phones => displaySearchResult(phones.data));
+        .then(phones => displaySearchResult(phones.data.slice(0, 20)));
 }
 
 // search results
@@ -18,7 +18,7 @@ const displaySearchResult = data => {
     phoneDetails.textContent = '';
 
     data.forEach(phone => {
-        // console.log(phone);
+        console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -30,8 +30,7 @@ const displaySearchResult = data => {
                 <p class="card-text">To know additional information, please click on details.</p>
             </div>
             <div onclick="loadPhoneDetails('${phone.slug}')" class="pb-2 d-flex justify-content-center">
-                <a class="fw-bold btn btn-outline-primary"
-                        href="#" role="button">Details</a>
+                <a class="fw-bold btn btn-outline-primary" role="button">Details</a>
             </div>
         </div>
         `;
@@ -57,9 +56,16 @@ const displayPhoneDetails = details => {
             <img src="${details.image}" class="card-img-top" alt="...">
             <div class="card-body">
                     <h5 class="card-title">${details.name}</h5>
-                    <p class="card-text">Sensors: ${details.mainFeatures.sensors}</p>
-                    <p class="card-text">Others: ${details.others}</p>
-                    <p class="card-text">${details.releaseDate}</p>
+                    <h6 class="card-title">Sensors:</h6>
+                    <p class="card-text">${details.mainFeatures.sensors}</p>
+                    <h6 class="card-title">Others:</h6>
+                    <p class="card-text">Bluetooth: ${details.others.Bluetooth}</p>
+                    <p class="card-text">GPS: ${details.others.GPS}</p>
+                    <p class="card-text">NFC: ${details.others.NFC}</p>
+                    <p class="card-text">Radio: ${details.others.Radio}</p>
+                    <p class="card-text">USB: ${details.others.USB}</p>
+                    <p class="card-text">WLAN: ${details.others.WLAN}</p>
+                    <p class="card-text">Release Date: ${details.releaseDate}</p>
             </div>
     `;
     phoneDetails.appendChild(div);
